5abcd62b9a898538910f4cd3f291331fd3c254b6
Evan Woods <22337004+efwoods@users.noreply.github.com>
2022-08-22 14:01:17 -0400

    commit

---
+++ prog.java
@@ -0,0 +1,30 @@
+public class prog {
+    public static void main(String[] args) {
+        int Minutes_Spent = 90;
+        float Total_pages = 991;
+        float pages_read = 15;
+        try{
+            pages_read = Integer.valueOf(args[0]);
+        } catch(Error e) {
+            pages_read = 15;
+            Minutes_Spent = 90;
+        }
+        System.out.println(pages_read);
+        float Percent_Complete = (pages_read/Total_pages) * 100;
+        float Remaining_Percent = 100 - Percent_Complete;
+        float RateOfCompletion_Percent_per_Minutes = Percent_Complete/Minutes_Spent;
+        float TimeToComplete = Remaining_Percent/RateOfCompletion_Percent_per_Minutes;
+
+        System.out.println("Percent Complete: " + Percent_Complete + "%");
+        
+        System.out.println("Remaining Percent: " + Remaining_Percent + "%");
+        System.out.println("Rate of Completion in percent per minute: " + RateOfCompletion_Percent_per_Minutes);
+
+        System.out.println("Time to complete:" + TimeToComplete/60 + " hours");
+        System.out.println("Time to complete:" + TimeToComplete + " minutes");
+        
+
+
+
+    }
+}
